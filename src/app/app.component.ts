79463affe04e7ca026036a0ba445482e89cc3131
吕俊涛 <lv_juntao@sina.com>
2018-07-16 16:47:24 +0800

    标志

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, AfterViewInit} from '@angular/core';
+import {e} from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-root',
@@ -6,13 +7,8 @@ import {Component, OnInit, AfterViewInit} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, AfterViewInit {
-  options = {
-    theme: 'vs-dark',
-    language: 'javascript'
-  };
-  code = `function hello() {
-	 alert('Hello world!');
-  }`;
+  options: any;
+  code: any;
   _editor: any;
 
   ngOnInit() {
@@ -22,8 +18,237 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
 
   onInit(editor) {
+
+    function getCode() {
+      return [
+        'class [Sun Mar 7 16:02:00 2004] [notice] Apache/1.3.29 (Unix) configured -- resuming normal operations',
+        '[Sun Mar 7 16:02:00 2004] [info] Server built: Feb 27 2004 13:56:37',
+        '[Sun Mar 7 16:02:00 2004] [notice] Accept mutex: sysvsem (Default: sysvsem)',
+        '[Sun Mar 7 16:05:49 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 16:45:56 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 17:13:50 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 17:21:44 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 17:23:53 2004] statistics: Use of uninitialized value in concatenation (.) or string at /home/httpd/twiki/lib/TWiki.pm line 528.',
+        '[Sun Mar 7 17:23:53 2004] statistics: Can\'t create file /home/httpd/twiki/data/Main/WebStatistics.txt - Permission denied',
+        '[Sun Mar 7 17:27:37 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 17:31:39 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 17:58:00 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 18:00:09 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 18:10:09 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 18:19:01 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 18:42:29 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 18:52:30 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 18:58:52 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 19:03:58 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 19:08:55 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 20:04:35 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 20:11:33 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 20:12:55 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 20:25:31 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 20:44:48 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 20:58:27 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 21:16:17 2004] [error] [client xx.xx.xx.xx] File does not exist: /home/httpd/twiki/view/Main/WebHome',
+        '[Sun Mar 7 21:20:14 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 21:31:12 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 21:39:55 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Sun Mar 7 21:44:10 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Mon Mar 8 01:35:13 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Mon Mar 8 01:47:06 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Mon Mar 8 01:59:13 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Mon Mar 8 02:12:24 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Mon Mar 8 02:54:54 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Mon Mar 8 03:46:27 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Mon Mar 8 03:48:18 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Mon Mar 8 03:52:17 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Mon Mar 8 03:55:09 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Mon Mar 8 04:22:55 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Mon Mar 8 04:24:47 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Mon Mar 8 04:40:32 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Mon Mar 8 04:55:40 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Mon Mar 8 04:59:13 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Mon Mar 8 05:22:57 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Mon Mar 8 05:24:29 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '[Mon Mar 8 05:31:47 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed',
+        '<11>httpd[31628]: [error] [client xx.xx.xx.xx] File does not exist: /usr/local/installed/apache/htdocs/squirrelmail/_vti_inf.html in 29-Mar 15:18:20.50 from xx.xx.xx.xx',
+        '<11>httpd[25859]: [error] [client xx.xx.xx.xx] File does not exist: /usr/local/installed/apache/htdocs/squirrelmail/_vti_bin/shtml.exe/_vti_rpc in 29-Mar 15:18:20.54 from xx.xx.xx.xx',
+      ].join('\n');
+    }
+
+    function getOption() {
+      return {
+        // Set defaultToken to invalid to see what you do not tokenize yet
+        // defaultToken: 'invalid',
+
+        keywords: [
+          'abstract', 'continue', 'for', 'new', 'switch', 'assert', 'goto', 'do',
+          'if', 'private', 'this', 'break', 'protected', 'throw', 'else', 'public',
+          'enum', 'return', 'catch', 'try', 'interface', 'static', 'class',
+          'finally', 'const', 'super', 'while', 'true', 'false'
+        ],
+
+        typeKeywords: [
+          'boolean', 'double', 'byte', 'int', 'short', 'char', 'void', 'long', 'float'
+        ],
+
+        operators: [
+          '=', '>', '<', '!', '~', '?', ':', '==', '<=', '>=', '!=',
+          '&&', '||', '++', '--', '+', '-', '*', '/', '&', '|', '^', '%',
+          '<<', '>>', '>>>', '+=', '-=', '*=', '/=', '&=', '|=', '^=',
+          '%=', '<<=', '>>=', '>>>='
+        ],
+
+        // we include these common regular expressions
+        symbols: /[=><!~?:&|+\-*\/\^%]+/,
+
+        // C# style strings
+        escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+
+        // The main tokenizer for our languages
+        tokenizer: {
+          root: [
+            // identifiers and keywords
+            [/[a-z_$][\w$]*/, {
+              cases: {
+                typeKeywords: 'typeKeywords',
+                keywords: 'keyword',
+                default: 'identifier'
+              }
+            }],
+            [/[A-Z][\w\$]*/, 'type.identifier'],  // to show class names nicely
+
+            // whitespace
+            {include: '@whitespace'},
+
+            // delimiters and operators
+            [/[{}()\[\]]/, '@brackets'],
+            [/[<>](?!symbols)/, '@brackets'],
+            [/symbols/, {
+              cases: {
+                operators: 'operator',
+                default: ''
+              }
+            }],
+
+            // @ annotations.
+            // As an example, we emit a debugging log message on these tokens.
+            // Note: message are supressed during the first load -- change some lines to see them.
+            [/@\s*[a-zA-Z_\$][\w\$]*/, {token: 'annotation', log: 'annotation token: $0'}],
+
+            // numbers
+            [/\d*\.\d+([eE][\-+]?\d+)?/, 'number.float'],
+            [/0[xX][0-9a-fA-F]+/, 'number.hex'],
+            [/\d+/, 'number'],
+
+            // delimiter: after number because of .\d floats
+            [/[;,.]/, 'delimiter'],
+
+            // strings
+            [/"([^"\\]|\\.)*$/, 'string.invalid'],  // non-teminated string
+            [/"/, {token: 'string.quote', bracket: '@open', next: '@string'}],
+
+            // characters
+            [/'[^\\']'/, 'string'],
+            [/(')(escapes)(')/, ['string', 'string.escape', 'string']],
+            [/'/, 'string.invalid']
+          ],
+
+          comment: [
+            [/[^\/*]+/, 'comment'],
+            [/\/\*/, 'comment', '@push'],    // nested comment
+            ['\\*/', 'comment', '@pop'],
+            [/[\/*]/, 'comment']
+          ],
+
+          string: [
+            [/[^\\"]+/, 'string'],
+            [/escapes/, 'string.escape'],
+            [/\\./, 'string.escape.invalid'],
+            [/"/, {token: 'string.quote', bracket: '@close', next: '@pop'}]
+          ],
+
+          whitespace: [
+            [/[ \t\r\n]+/, 'white'],
+            [/\/\*/, 'comment', '@comment'],
+            [/\/\/.*$/, 'comment'],
+          ],
+        },
+      };
+    }
+
     this._editor = editor;
-    const line = this._editor.getPosition();
-    console.log(line);
+    // Register a new language
+    monaco.languages.register({id: 'mySpecialLanguage'});
+
+// Register a tokens provider for the language
+    console.log(getOption().tokenizer);
+    const ops: any = {
+      tokenizer: getOption().tokenizer
+    };
+    monaco.languages.setMonarchTokensProvider('mySpecialLanguage', ops);
+
+// Define a new theme that constains only rules that match this language
+    const setOp: any = {
+      base: 'vs',
+      inherit: false,
+      rules: [
+        {token: 'custom-info', foreground: '808080'},
+        {token: 'custom-error', foreground: 'ff0000', fontStyle: 'bold'},
+        {token: 'custom-notice', foreground: 'FFA500'},
+        {token: 'custom-date', foreground: '008800'},
+        {token: 'number', foreground: 'ff0000'},
+        {token: 'keyword', foreground: 'ff0000'},
+        {token: 'typeKeywords', foreground: 'ff0000'}
+      ]
+    };
+    monaco.editor.defineTheme('myCoolTheme', setOp);
+
+// Register a completion item provider for the new language
+    monaco.languages.registerCompletionItemProvider('mySpecialLanguage', {
+      provideCompletionItems: () => {
+        return [
+          {
+            label: 'simpleText',
+            kind: monaco.languages.CompletionItemKind.Text
+          }, {
+            label: 'testing',
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            insertText: {
+              value: 'testing(${1:condition})'
+            }
+          },
+          {
+            label: 'ifelse',
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            insertText: {
+              value: [
+                'if (${1:condition}) {',
+                '\t$0',
+                '} else {',
+                '\t',
+                '}'
+              ].join('\n')
+            },
+            documentation: 'If-Else Statement'
+          }
+        ];
+      }
+    });
+    /*获取位置*/
+    const line = editor.getPosition();
+    const range = new monaco.Range(line.lineNumber, 1, line.lineNumber, 1);
+    const id = {major: 1, minor: 1};
+    const text = getCode();
+    const op = {identifier: id, range: range, text: text, forceMoveMarkers: true};
+    editor.executeEdits('source', [op]);
+    const model = editor.getModel(this._editor);
+    monaco.editor.setModelLanguage(model, 'mySpecialLanguage');
+    monaco.editor.setTheme('myCoolTheme');
+    this.onChange();
   }
+
+  onChange() {
+    this._editor.onDidChangeModelContent((e) => {
+    });
+  }
+
 }
